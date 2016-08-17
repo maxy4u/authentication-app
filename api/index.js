7f65ee308dfd0ff20e5d4493f8124c42ba95dd89
maxy4u <khurana.g@hotmail.com>
2016-08-17 14:01:13 -0300

    first commit

---
+++ api/index.js
@@ -0,0 +1,133 @@
+// Require deps
+var express = require('express');
+var expressJwt = require('express-jwt');
+var jwt = require('jsonwebtoken');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var fs = require('fs');
+
+
+// Database
+var dbFile = './user.json';
+if (fs.existsSync(__dirname + '/user.local.json')) {
+  dbFile = './user.local.json';
+}
+var user = require(dbFile);
+var userPassword = user.password;
+delete user.password;
+
+// setup server
+var app = express();
+
+app.use(bodyParser());
+
+// setup jwt
+var jwtSuperSecretCode = 'super-secret-key';
+var validateJwt = expressJwt({secret: jwtSuperSecretCode});
+// serve app from server
+app.use(express.static('../frontend'));
+app.use('/', function (req, res, next) {
+  if (req.originalUrl === '/login') {
+    next();
+  } else {
+    if(req.query && req.query.hasOwnProperty('access_token')) {
+      req.headers.authorization = 'Bearer ' + req.query.access_token;
+    }
+    validateJwt(req, res, next);
+  }
+});
+
+
+// setup cors
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  next();
+});
+
+
+// setup passport
+passport.use(new LocalStrategy(function(username, password, done) {
+  if (username === user.username && password === userPassword) {
+    return done(null, user);
+  } else {
+    done(null, false, { message: 'Incorrect username or password' });
+  }
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.username);
+});
+
+passport.deserializeUser(function(username, done) {
+  if (username === user.username) {
+    done(null, user);
+  } else {
+    done('No user with username ' + username);
+  }
+});
+
+app.use(passport.initialize());
+
+
+
+// setup routes
+app.post('/login', function(req, res, next) {
+  passport.authenticate('local', function(err, user, info) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.json(404, 'No user found...');
+    }
+    req.logIn(user, function(err) {
+      if (err) {
+        return next(err);
+      }
+      var token = jwt.sign({
+        username: user.username
+      }, jwtSuperSecretCode);
+      return res.json(200, { token: token, user: user });
+    });
+  })(req, res, next);
+});
+
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.json(200, { success: true });
+});
+
+app.get('/users/me', function(req, res) {
+  if (req.user) {
+    res.json(user);
+  } else {
+    res.json(403, { message: 'Not authorized' });
+  }
+});
+
+var funnyPicIndex = Math.floor(Math.random()*12);
+function getNextFunnyPic() {
+  console.log(funnyPicIndex	);
+  funnyPicIndex++;
+  if (funnyPicIndex > 12) {
+    funnyPicIndex = 0;
+  }
+  return __dirname + '/funny-pics/' + funnyPicIndex + '.jpg';
+}
+
+app.get('/funny-pic', function(req, res) {
+  if (req.user) {
+    res.sendfile(getNextFunnyPic());
+  } else {
+    res.json(403, { message: 'Not authorized' });
+  }
+});
+
+// listen on port
+var server = app.listen(3000, function() {
+  var addy = server.address();
+  console.log('Server listening at: ', addy.address + ':' + addy.port);
+});
---
+++ frontend/script.js
@@ -0,0 +1,165 @@
+(function() {
+  var app = angular.module('ng.jwt.workshop', []);
+
+  app.constant('Firebase', Firebase);
+  app.constant('API_BASE', 'http://localhost:3000/');
+  app.factory('AuthToken', function($window) {
+    var tokenKey = 'user-token';
+    var storage = $window.localStorage;
+    var cachedToken;
+    return {
+      isAuthenticated: isAuthenticated,
+      setToken: setToken,
+      getToken: getToken,
+      clearToken: clearToken
+    };
+    function setToken(token) {
+      cachedToken = token;
+      storage.setItem(tokenKey, token);
+    }
+    function getToken() {
+      if (!cachedToken) {
+        cachedToken = storage.getItem(tokenKey);
+      }
+      return cachedToken;
+    }
+    function clearToken() {
+      cachedToken = null;
+      storage.removeItem(tokenKey);
+    }
+    function isAuthenticated() {
+      return !!getToken();
+    }
+  });
+
+  app.factory('AuthInterceptor', function ($rootScope, $q, AuthToken) {
+    return {
+      request: function (config) {
+        var token = AuthToken.getToken();
+        if (token) {
+          config.headers = config.headers || {};
+          config.headers.Authorization = 'Bearer ' + token;
+        }
+        return config;
+      },
+      response: function (response) {
+        if (response.status === 401) {
+          console.warn('user not authenticated', response);
+          // handle the case where the user is not authenticated
+        }
+        return response || $q.when(response);
+      }
+    };
+  });
+  
+  app.controller('MainCtrl', function($scope, $http, API_BASE, $timeout, AuthToken, $window, Firebase) {
+
+    $scope.getMe = function() {
+      $http.get(API_BASE + 'users/me').then(function success(response) {
+        $scope.user = response.data;
+        $scope.alreadyLoggedIn = true;
+        showAlert('info', 'Hello', 'and welcome back ' + $scope.user.username + '!');
+      }, function error() {
+        AuthToken.clearToken();
+        console.log('getting user error', arguments);
+      }).finally(function() {
+        $scope.meRequestComplete = true;
+      });
+    };
+
+    if (AuthToken.isAuthenticated()) {
+      $scope.getMe();
+    } else {
+      $scope.meRequestComplete = true;
+    }
+
+    $scope.login = function(username, password) {
+      $scope.badCreds = false;
+      $http({
+        url: API_BASE + 'login',
+        method: 'POST',
+        data: {
+          username: username,
+          password: password
+        }
+      }).then(function success(response) {
+        AuthToken.setToken(response.data.token);
+        $scope.user = response.data.user;
+        $scope.noPicture = true;
+        $scope.alreadyLoggedIn = true;
+        showAlert('success', 'Hey there!', 'Welcome ' + $scope.user.username + '!');
+      }, function error(response) {
+        if (response.status === 404) {
+          $scope.badCreds = true;
+          showAlert('danger', 'Whoops...', 'Do I know you?');
+        } else {
+          showAlert('danger', 'Hmmm....', 'Problem logging in! Sorry!');
+        }
+      });
+    };
+    $scope.toggleFunnyPicture = function() {
+	  console.log('called')
+      $scope.noPicture = false;
+      if ($scope.funnyPictureUrl !== "") {
+        $scope.funnyPictureUrl = "";
+      } else {
+        $scope.funnyPictureUrl = API_BASE + 'funny-pic?access_token=' + $window.encodeURIComponent(AuthToken.getToken())+'&time='+new Date();
+      }
+    };
+
+    $scope.logout = function() {
+      $scope.funnyPictureUrl = null;
+      AuthToken.clearToken();
+      $scope.user = null;
+      showAlert('info', 'Goodbye!', 'Have a great day!');
+    };
+
+    $scope.sendFeedback = function(feedback) {
+      var d = new Date();
+      var formattedDate = d.getFullYear() + '-' + addZero(d.getMonth() + 1) + '-' + addZero(d.getDate());
+      var url = 'https://ng-jwt-workshop.firebaseio.com/' + formattedDate;
+      var feedbackRef = new Firebase(url);
+      feedbackRef.push(feedback);
+      showAlert('info', 'Sent!', 'Thanks for the feedback!');
+      $scope.showFeedback = false;
+    };
+    function addZero(num) {
+      return (num < 10 ? '0' : '') + num;
+    }
+
+    $scope.showAppropriateAlertForScore = function(feedback) {
+      if (feedback.score !== 0 && !feedback.score) {
+        return;
+      }
+      var timeout = 5000;
+      if (feedback.score <= 3) {
+        showAlert('danger', ['Ouch', feedback.name].join(' ').trim() + '!', 'That hurt... I hope next time I can do better for you... Suggestions welcome.', timeout);
+      } else if (feedback.score <= 6) {
+        showAlert('warning', ['Huh', feedback.name].join(' ').trim() + '...', 'I appreciate any constructive suggestions you have!', timeout);
+      } else if (feedback.score <= 9) {
+        showAlert('info', ['Great', feedback.name].join(' ').trim() + '!', 'Much appreciated, I\'d love to know what I can improve to make it perfect!', timeout);
+      } else {
+        showAlert('success', ['Wow', feedback.name].join(' ').trim() + '!', 'Gee! Thanks for the perfect score! What made it so good?', timeout);
+      }
+    };
+
+    var alertTimeout;
+    function showAlert(type, title, message, timeout) {
+      $scope.alert = {
+        hasBeenShown: true,
+        show: true,
+        type: type,
+        message: message,
+        title: title
+      };
+      $timeout.cancel(alertTimeout);
+      alertTimeout = $timeout(function() {
+        $scope.alert.show = false;
+      }, timeout || 1500);
+    }
+  });
+
+  app.config(function ($httpProvider) {
+    $httpProvider.interceptors.push('AuthInterceptor');
+  });
+})();
